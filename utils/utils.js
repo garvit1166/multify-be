317a4c381a1d9c1b9510d44f8498631d04a987d7
garvit <garvitchutani07@gmail.com>
2024-04-04 20:00:21 +0530

    improve:updated the getlast10lines and getnew function

--- utils/utils.js
+++ utils/utils.js
@@ -1,64 +1,35 @@
 const fs = require("fs");
-const readline = require("readline");
 
-const getLast = (filename) => {
-    return new Promise((resolve, reject) => {
-        fs.accessSync(filename, error => {
-            reject( error )
-        });
-        let lines = [];
-        let r1 = readline.createInterface({
-            input: fs.createReadStream(filename),
-            output: process.stdout,
-            terminal: false
-        });
-        
-        r1.on('error', error => {
-            reject(error);
-        });
 
-        r1.on('line', input => {
-            lines.push(input)
-        });
-    
-        r1.on('close', () => {
-            lastLine = lines.length > 0 ? lines[lines.length-1] : '';
-            resolve(lines.slice(-10));
-        });
-    })
+const getLastTenLines = (filename) => {
+    try {
+        const fileContent = fs.readFileSync(filename, 'utf8');
+        const lines = fileContent.split('\n');
+        const lastTenLines = lines.slice(-10);
+        return lastTenLines;
+    } catch (error) {
+        console.error('Error:', error);
+        return [];
+    }
 }
 
-const getNew = (filename) => {
-    return new Promise((resolve, reject) => {
-        fs.accessSync(filename, error => {
-            reject( error )
-        });
+const getNewLinesAdded = (filename, previousContent) => {
+    try {
+        const fileContent = fs.readFileSync(filename, 'utf8');
+        const newContent = fileContent.split('\n');
+        const previousContentArr = previousContent.split('\n');
 
-        let lines = [];
-        let register = false;
-        let r1 = readline.createInterface({
-            input: fs.createReadStream(filename),
-            output: process.stdout,
-            terminal: false
-        });
+        const newLinesAdded = newContent.filter(line => !previousContentArr.includes(line));
 
-        r1.on('line', input => {
-            // console.log(input)
-            if (register || lastLine == '') {
-                lines.push(input)
-            } else if (input === lastLine) {
-                register = true;
-            }
-        });
-    
-        r1.on('close', () => {
-            lastLine = lines.length > 0 ? lines[lines.length-1] : '';
-            resolve(lines);
-        });
-    })
+        return newLinesAdded;
+    } catch (error) {
+        console.error('Error:', error);
+        return [];
+    }
 }
+
 
 module.exports = {
-    getLast,
-    getNew
+    getLastTenLines,
+    getNewLinesAdded
 }
